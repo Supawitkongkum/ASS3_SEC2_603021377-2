4c6b2d0eece0d19c8fa9c4bb13316be6b8aab0e6
Supawitkongkum <53934250+Supawitkongkum@users.noreply.github.com>
2019-08-15 23:52:33 +0700

    ASS3_SEC2_603021377-2
    ASS3_SEC2_603021377-2

---
+++ ASS3_SEC2_6030213772/app/src/main/java/com/example/ass3_sec2_603021377_2/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.ass3_sec2_603021377_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
